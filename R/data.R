e2414e1fbcdc937bcd062666a55380d49d3cf925
gaochenyin <781747089@qq.com>
2022-10-12 11:19:23 -0400

    update

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Simulation results for beta0 = c(0,1,1,1) and psi0 = c(0,1,1) in Yang et al., (2022), Section 4
+#'
+#' @format ##`elastic_psi011_lists`
+#' A data list with length 10,
+#' each one associated with different strength of unmeasured confounding (e.g., 0, ..., 2)
+"elastic_psi011_lists"
+
+#' Summary results for beta0 = c(0,1,1,1) and psi0 = c(0,1,1) in Yang et al., (2022), Table 1
+#'
+#' @format ##`summary.tables`
+#' A data frame with the coverage rates and confidence intervals
+#' with different strength of unmeasured confounding
+"summary.tables"
